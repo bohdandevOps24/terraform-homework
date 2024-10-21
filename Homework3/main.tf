fb405e27a8ebef971f9084098a1b4bd60d3055f3
EC2 Default User <ec2-user@ip-172-31-37-129.eu-central-1.compute.internal>
2024-10-21 02:10:53 +0000

    homework3

---
+++ Homework3/main.tf
@@ -0,0 +1,45 @@
+data "aws_ami" "amazon-linux-2" {
+ most_recent = true
+
+
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+}
+
+
+# Specify 3 availability zones from the region
+variable "availability_zones" {
+  default = ["us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+# Specify 3 web names
+variable "webName" {
+  default = ["web-1", "web-2", "web-3"]
+}
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.amazon-linux-2.id
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+  count = 3
+  user_data =  file("apache2.sh")
+  availability_zone = var.availability_zones[count.index]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+ tags = {
+    Name = "web - ${count.index +1}"
+  }
+}
---
+++ Homework3/provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "Bastion-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+
+  
+}
+
